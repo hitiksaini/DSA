b7664a5f0eb13892e84ce306fa764daecd5efcb3
Hitik Saini <hitik9045saini@gmail.com>
2020-09-19 20:31:52 +0530

    commented a fn

--- Sorting Algorithms/Binary_Search.py
+++ Sorting Algorithms/Binary_Search.py
@@ -19,26 +19,26 @@ else:
     print("Not found")
     
  # recursive approach
- un-comment the part to see recursive method
+#  un-comment the part to see recursive method
 # recursive approach
-def binarySearch(array, x, low, high):
-    if high >= low:
-        mid = low + (high - low)//2
-        # If found at mid, then return it
-        if array[mid] == x:
-            return mid
-        # Search the left half
-        elif array[mid] > x:
-            return binarySearch(array, x, low, mid-1)
-        # Search the right half
-        else:
-            return binarySearch(array, x, mid + 1, high)
-    else:
-        return -1
-array = [3, 4, 5, 6, 7, 8, 9]
-x = 4
-result = binarySearch(array, x, 0, len(array)-1)
-if result != -1:
-    print("Element is present at index " + str(result))
-else:
-    print("Not found")
+# def binarySearch(array, x, low, high):
+#     if high >= low:
+#         mid = low + (high - low)//2
+#         # If found at mid, then return it
+#         if array[mid] == x:
+#             return mid
+#         # Search the left half
+#         elif array[mid] > x:
+#             return binarySearch(array, x, low, mid-1)
+#         # Search the right half
+#         else:
+#             return binarySearch(array, x, mid + 1, high)
+#     else:
+#         return -1
+# array = [3, 4, 5, 6, 7, 8, 9]
+# x = 4
+# result = binarySearch(array, x, 0, len(array)-1)
+# if result != -1:
+#     print("Element is present at index " + str(result))
+# else:
+#     print("Not found")
